fff9b06e51215d68e5417fadfcaa9092cd4d183d
Yenson Lau <y.lau@columbia.edu>
2018-05-08 14:58:25 -0400

    Fixed initpkg.m.

--- initpkg.m
+++ initpkg.m
@@ -12,25 +12,36 @@ function initpkg(rm)
 
 % Directories + subdirectories to add to path
 dirs = {
-    '.'
-    'utils'
+    '.',        false;
+    'utils',    true;
 };
 
 warning('OFF', 'MATLAB:addpath:DirNotFound');
 warning('OFF', 'MATLAB:rmpath:DirNotFound');
 
 if nargin < 1 || isempty(rm);  rm = false;  end
-if rm;  op = @rmpath;  else;  op = @addpath;  end
 
 pkgpath = mfilename('fullpath');
 d = strsplit(pkgpath, {'\','/'});
 pkgpath = pkgpath(1:end-numel(d{end}));
 
-for d = 1:numel(dirs)
-    op(genpath([pkgpath dirs{d}]));
+for d = 1:size(dirs,1)
+    pathop([pkgpath dirs{d,1}], rm, dirs{d,2});
 end
 
 warning('ON', 'MATLAB:addpath:DirNotFound');
 warning('ON', 'MATLAB:rmpath:DirNotFound');
 
+end
+
+function pathop(path, optype, recursive)
+    if recursive
+        path = genpath(path);
+    end
+    
+    if optype
+        rmpath(path);
+    else
+        addpath(path);
+    end
 end
